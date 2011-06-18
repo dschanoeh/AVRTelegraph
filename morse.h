3db3141a012ec4805a3b9c291fb1148039d3c65c
Jan-Niklas Meier <dschanoeh@googlemail.com>
2011-06-18 13:07:16 +0200

    more moderate time settings

--- morse.h
+++ morse.h
@@ -4,14 +4,21 @@
 #define F_CPU 16000000
 
 /* time quanta of symbols in ms */
-#define TIME_QUANTA 100
+#define TIME_QUANTA 400
 
 /* length of symbols (in time quanta) */
+/* defaults:
+ * #define TIME_DOT 1
+ * #define TIME_DASH 3
+ * #define TIME_CHARACTER_PAUSE 3
+ * #define TIME_SYMBOL_PAUSE 1
+ * #define TIME_WORD_PAUSE 7
+ */
 #define TIME_DOT 1
-#define TIME_DASH 3
-#define TIME_CHARACTER_PAUSE 3
-#define TIME_SYMBOL_PAUSE 1
-#define TIME_WORD_PAUSE 7
+#define TIME_DASH 5
+#define TIME_CHARACTER_PAUSE 7
+#define TIME_SYMBOL_PAUSE 2
+#define TIME_WORD_PAUSE 15
 
 #define ON 1
 #define OFF 0
